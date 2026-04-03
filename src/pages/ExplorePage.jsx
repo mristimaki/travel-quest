98e2cdbbb150def64810ff86261bf21daa5bfc7e
m.ristimaki <rosanna.ristimaki@gmail.com>
2026-04-03 21:27:49 +0200

    Add data fetching with loading and error state in ExplorePage

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -1,4 +1,29 @@
+import { useEffect, useState } from "react"
+
+
 function ExplorePage() {
+    const [destinations, setDestinations] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setLoading(true);  
+
+        fetch('https://restcountries.com/v3.1/all?fields=name,capital,region,flags,population,cca3')
+            .then(response => response.json())
+            .then(data => {
+                setDestinations(data)
+            })
+            .catch(err => {
+                setError(err.message)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+    }, []);
+
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Something went wrong: {error}</p>
     return <h1>Explore</h1>
 }
 
